2f7b76931a94ac48bf537f11fdcff7cdfd7544d1
Grohmur <zverma92@gmail.com>
2021-12-12 12:15:51 +0300

    All actions in one

--- colorSlider/ViewController.swift
+++ colorSlider/ViewController.swift
@@ -32,29 +32,12 @@ class ViewController: UIViewController {
         sliderBlue.value = 0
         sliderGreen.value = 0
     }
-
+    
     @IBAction func redSliderAction() {
         valueRed.text = String(round(sliderRed.value * 100) / 100)
         colorView.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                            green: CGFloat(sliderGreen.value),
-                            blue: CGFloat(sliderBlue.value),
-                            alpha: 1)
+                                      green: CGFloat(sliderGreen.value),
+                                      blue: CGFloat(sliderBlue.value),
+                                            alpha: 1)
     }
-    @IBAction func greenSliderAction() {
-        valueGreen.text = String(round(sliderGreen.value * 100) / 100)
-        colorView.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                            green: CGFloat(sliderGreen.value),
-                            blue: CGFloat(sliderBlue.value),
-                            alpha: 1)
-    }
-    @IBAction func blueSliderAction() {
-        valueBlue.text = String(round(sliderBlue.value * 100) / 100)
-        colorView.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                            green: CGFloat(sliderGreen.value),
-                            blue: CGFloat(sliderBlue.value),
-                            alpha: 1)
-    }
-    
-
 }
-
